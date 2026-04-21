3eaaea30bc40fd11e0e20216a4875f8551d1708d
M&M Nexus <mmnexus.global@gmail.com>
2026-04-21 16:22:43 -0300

    Primera versión del Dashboard y Agente AI

--- app/page.tsx
+++ app/page.tsx
@@ -157,7 +157,7 @@ export default function Dashboard() {
                       </div>
                     )}
                   </div>
-                </div>            </div>
+                </div>
               </div>
             </div>
 
